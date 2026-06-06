4aa46419975c2ff5ae5b1dbec2efdfab0aae25de
hashem <h03029283@gmail.com>
2026-06-06 10:29:14 +0300

    Create join_handel.rs

---
+++ adapters/src/join_handel.rs
@@ -0,0 +1,48 @@
+#[cfg(not(target_arch = "wasm32"))]
+pub mod m {
+    use crate::prelude::*;
+
+    pub struct S<T>(pub tokio::task::JoinHandle<T>);
+
+    impl<T> JoinHandel for S<T> {
+        fn abort(self) {
+            self.0.abort();
+        }
+    }
+
+    impl<T> S<T> {
+        pub fn new(t: tokio::task::JoinHandle<T>) -> Self {
+            Self(t)
+        }
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+pub mod m {
+    use crate::prelude::*;
+    use futures::lock::Mutex;
+    use std::sync::Arc;
+    use wasm_bindgen_futures::spawn_local;
+
+    pub struct S<T> {
+        pub output: Arc<Mutex<Option<T>>>,
+        aborter: mpsc_sender::m::S<()>,
+    }
+
+    impl<T> JoinHandel for S<T> {
+        fn abort(self) {
+            spawn_local(async move {
+                self.aborter.send(()).await;
+            });
+        }
+    }
+
+    impl<T> S<T> {
+        pub fn new(aborter: mpsc_sender::m::S<()>) -> Self {
+            Self {
+                output: Arc::default(),
+                aborter,
+            }
+        }
+    }
+}
