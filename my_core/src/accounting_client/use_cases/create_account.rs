0cf8bbf6af33318ddc589df40aee168636db664d
hashem <h03029283@gmail.com>
2026-07-27 16:52:49 +0300

    Update create_account.rs

--- my_core/src/accounting_client/use_cases/create_account.rs
+++ my_core/src/accounting_client/use_cases/create_account.rs
@@ -11,7 +11,6 @@ use crate::accounting_domain::request_response;
 use crate::accounting_domain::utility::resource_utils;
 use crate::accounting_domain::utility::types;
 use crate::accounting_domain::utility::types::MyErrorTrait;
-use crate::mbg;
 use crate::utility::traits;
 use crate::utility::traits::JoinHandle;
 use crate::utility::traits::Receiver;
